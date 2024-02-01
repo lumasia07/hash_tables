5b65ea2f88c2983d66db71495c773e77318c6aa0
lumasia stancelaus <lumasiastancelaus@gmail.com>
2024-02-01 13:11:10 +0300

    Input Handling

--- main.c
+++ main.c
@@ -1,9 +1,15 @@
 #include "main.h"
 
 /* Main File */
-int main()
+int main(int ac, char *av[])
 {
-	char input[50] = "Cau";
+	if (ac != 2)
+	{
+		printf("Usage: %s <string>\n", av[0]);
+		return (1);
+	}
+
+	char *input = av[1];
 	unsigned long hash_value = hash_function(input);
 
 	printf("Hash value: %lu\n", hash_value);
