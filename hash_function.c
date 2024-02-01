224f27a5e2970aef6cb2acd16e17646f35d7cf9f
lumasia stancelaus <lumasiastancelaus@gmail.com>
2024-02-01 12:07:10 +0300

    hash function

---
+++ hash_function.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+unsigned long hash_function(char* str)
+{
+	int j;
+	unsigned long i = 0; /* Stores hash value */
+
+	/* Iterates through each character in string inputed */
+	for (j = 0; str[j]; j++)
+		i += str[j]; /* adds ASCII value to hash value */
+
+	return i % CAPACITY; /* Calculates remainder & returns hash value */
+}
