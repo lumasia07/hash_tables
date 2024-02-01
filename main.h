7b9da4b6cee6f7bce60177a47310bb70f2a8e933
lumasia stancelaus <lumasiastancelaus@gmail.com>
2024-02-01 13:16:53 +0300

    hash table struct

--- main.h
+++ main.h
@@ -8,5 +8,14 @@
 /* Hash function */
 unsigned long hash_function(char* str);
 
+/* Structure to define a Hashtable */
+typedef struct hash_table
+{
+	/* Array of pointers to items */
+	ht_item** items;
+	int size;
+	int count;
+} hash_table;
+
 
 #endif /* MAIN_H */
