b343e687408b9156b34061a758c696bd6fc47d18
lumasia stancelaus <lumasiastancelaus@gmail.com>
2024-02-01 13:27:40 +0300

    define item

--- main.h
+++ main.h
@@ -8,6 +8,13 @@
 /* Hash function */
 unsigned long hash_function(char* str);
 
+/* Structure to define Hashtable Item */
+typedef struct ht_item
+{
+	char *key;
+	char * value;
+} ht_item;
+
 /* Structure to define a Hashtable */
 typedef struct hash_table
 {
